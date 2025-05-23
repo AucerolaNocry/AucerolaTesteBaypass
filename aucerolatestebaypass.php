a83359a3300b606f89c54408bce63f5eeff4bd94
AucerolaNocry <setterurlk@gmail.com>
2025-05-23 20:35:17 -0300

    Update aucerolatestebaypass.php

--- aucerolatestebaypass.php
+++ aucerolatestebaypass.php
@@ -1,11 +1,16 @@
 function ofuscarLogs() {
-    // Preenche o logcat com dados aleatórios
-    shell_exec("adb shell logcat -c && for i in {1..500}; do logcat -s 'SYS_' -v brief -t ".date('m-d')." ".rand(10,23).":".rand(10,59).":".rand(10,59)." FAKE_LOG: System_Event_".md5(rand())."; done");
-    
-    // Sobrescreve os logs atuais
-    shell_exec("adb shell logcat -b all -c");
-    
-    // Cria logs falsos com timestamps antigos
-    $old_date = date('m-d H:i:s', strtotime('-3 days'));
-    shell_exec("adb shell am broadcast -a android.intent.action.BOOT_COMPLETED --es 'TIMESTAMP' '$old_date'");
-}
+    global $verde, $bold, $cln;
+
+    // Limpa logs antigos
+    shell_exec("adb shell logcat -c");
+
+    // Gera entradas falsas de log com mensagem aleatória
+    for ($i = 0; $i < 300; $i++) {
+        $fakeTime = date('m-d H:i:s', strtotime("-".rand(2,5)." days ".rand(1,23)." hours ".rand(0,59)." minutes"));
+        $msg = "FAKE_LOG_" . substr(md5(rand()), 0, 6);
+        shell_exec("adb shell log -t 'SYS_EVENT' -p i '[LOG $fakeTime] $msg'");
+    }
+
+    // Confirmação final
+    echo $bold.$verde."[✓] Logcat preenchido com ruído falso e logs ofuscados!\n".$cln;
+        }
